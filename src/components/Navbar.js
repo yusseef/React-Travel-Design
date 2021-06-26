05031283d4f8f2909cddd261d1d9fac0a78a51b8
yusseef <yussef.raouf11@gmail.com>
2021-06-26 12:20:33 +0200

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,34 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 const Navbar = () => {
+    const[click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
     return (
         <>
           <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                    SAFRNY <i class="fab fa-typo3"/>
+                    SAFRNY <i class="fas fa-plane"/>
                 </Link>
-
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-items'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}> Home </Link>
+                    </li>
+                    <li className='nav-items'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}> Services </Link>
+                    </li>
+                    <li className='nav-items'>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}> Products </Link>
+                    </li>
+                    <li className='nav-items'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}> Signup </Link>
+                    </li>
+                </ul>
+                
             </div>
             </nav>  
         </>
